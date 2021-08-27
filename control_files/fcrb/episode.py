01163ee4bf86910673c81a6d0a14fa23dffe6d7b
skinnypigeon <euan.blackledge@soprasteria.com>
2021-08-27 10:01:03 +0100

    working on new dv implementation

---
+++ control_files/fcrb/episode.py
@@ -0,0 +1,23 @@
+fcrb_episode_hubs = {
+    'table': 'fcrb.episode',
+    'hubs': [
+        {
+            'hub': 'hub_time',
+            'keys': [
+                'falnr', 
+                'pernr', 
+                'einri', 
+                'patnr'
+            ]
+        },
+        {
+            'hub': 'hub_time',
+            'keys': [
+                'falnr', 
+                'pernr', 
+                'einri', 
+                'patnr'
+            ]
+        }
+    ]
+}
