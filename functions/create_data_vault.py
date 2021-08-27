38ba86a142ee93f6724983af7a00d0f497c1f306
skinnypigeon <euan.blackledge@soprasteria.com>
2021-08-27 15:14:27 +0100

    abstracted to functions

--- functions/create_data_vault.py
+++ functions/create_data_vault.py
@@ -2,25 +2,45 @@ from sqlalchemy import schema
 from functions.fcrb_data_vault import fcrb_data_vault
 from control_files.fcrb_keys_and_sats import fcrb_keys, fcrb_sats
 import pandas as pd
+pd.options.mode.chained_assignment = None
 
-def create_hubs(keys):
-    hub_time = pd.DataFrame(columns=keys)
-    hub_person = pd.DataFrame(columns=keys)
-    hub_object = pd.DataFrame(columns=keys)
-    hub_location = pd.DataFrame(columns=keys)
-    hub_event = pd.DataFrame(columns=keys)
-    return hub_time, hub_person, hub_object, hub_location, hub_event
+# def create_hubs(keys):
+#     hub_time = pd.DataFrame(columns=keys)
+#     hub_person = pd.DataFrame(columns=keys)
+#     hub_object = pd.DataFrame(columns=keys)
+#     hub_location = pd.DataFrame(columns=keys)
+#     hub_event = pd.DataFrame(columns=keys)
+#     return hub_time, hub_person, hub_object, hub_location, hub_event
 
-def create_key_dict(all_keys):
-    key_dict = {}
-    for key in all_keys:
-        key_dict[key] = ''
-    return key_dict
+# def create_key_dict(all_keys):
+#     key_dict = {}
+#     for key in all_keys:
+#         key_dict[key] = ''
+#     return key_dict
 
-def fill_hubs(fcrb_keys, row):
-    print(row)
-    key_dict = create_key_dict(fcrb_keys)
+# def fill_hubs(fcrb_keys, row):
+#     print(row)
+#     key_dict = create_key_dict(fcrb_keys)
 
+
+def get_table_and_table_keys(data, source_table_name):
+    table = data[source_table_name]
+    table_keys = []
+    for key in fcrb_keys:
+        if key in table.columns:
+            table_keys.append(key)
+    return table, table_keys
+    
+def get_remaining_fields(table, table_keys):
+    remaining_columns = [column for column in table.columns if column not in set(table_keys)]
+    return table[remaining_columns]
+
+def build_hubs(table, table_keys):
+    hub_fields = table[table_keys]
+    for key in fcrb_keys:
+        if key not in hub_fields.columns:
+            hub_fields.loc[:, key] = None
+    return hub_fields
 
 def create_data_vault(source_data, body):
     """This is a single hospital's full data set"""
@@ -29,20 +49,16 @@ def create_data_vault(source_data, body):
 
         """This is a single table from the data set and picks the keys"""
         for source_table_name in data.keys():
-            table = data[source_table_name]
-            table_keys = []
-            for key in fcrb_keys:
-                if key in table.columns:
-                    table_keys.append(key)
+            table, table_keys = get_table_and_table_keys(data, source_table_name)
 
             """Fill the hubs with the values for the primary keys"""
-            # [fill_hubs(fcrb_keys, row) for row in zip((table[table_key] for table_key in table_keys))]
-            # print([x for x in [table[table_key] for table_key in table_keys]])
-            # print([table_key for table_key in table_keys])
-            remaining_columns = [column for column in table.columns if column not in set(table_keys)]
-            remaining_fields = table[remaining_columns]
+            remaining_fields = get_remaining_fields(table, table_keys)
+            print("REMAINING FIELDS")
             print(remaining_fields)
+            print("\n")
 
-            hub_fields = table[table_keys]
+            hub_fields = build_hubs(table, table_keys)
+            print("HUB_FIELDS")
             print(hub_fields)
+            print("\n")
 
