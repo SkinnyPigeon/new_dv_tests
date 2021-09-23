d166c02f58486fe451edad9fddc9c96ec93d0e2e
skinnypigeon <euan.blackledge@soprasteria.com>
2021-09-23 14:30:47 +0100

    updated other keys

--- refactored/data_vault/keys/fcrb_keys.py
+++ refactored/data_vault/keys/fcrb_keys.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column, BigInteger, String
 from refactored.data_vault.keys.id_column import id_column
-def fcrb_keys(start_value):
+def fcrb_keys(start_value, table_name, metadata=None, schema=None):
     return [
-        id_column("id", primary=True, start_value=start_value),
+        id_column("id", table_name=table_name, metadata=metadata, schema=schema, hub=True, primary=True, start_value=start_value),
         Column("einri", String(4)),
         Column("patnr", BigInteger),
         Column("falnr", String(10)),
--- refactored/data_vault/keys/zmc_keys.py
+++ refactored/data_vault/keys/zmc_keys.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, BigInteger, String
 from refactored.data_vault.keys.id_column import id_column
-def zmc_keys(start_value):
+def zmc_keys(start_value, table_name, metadata=None, schema=None):
     return [
-        id_column("id", primary=True, start_value=start_value),
+        id_column("id", table_name=table_name, metadata=metadata, schema=schema, hub=True, primary=True, start_value=start_value),
         Column("patnr", BigInteger)
     ]
